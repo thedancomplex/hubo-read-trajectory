c91ae44082876786b1f126fef06a8676c8dda005
Daniel M. Lofaro <dan@danlofaro.com>
2012-12-14 00:43:01 -0500

    back to normal

--- src/hubo-read-trajectory.c
+++ src/hubo-read-trajectory.c
@@ -232,6 +232,8 @@ int runTraj(char* s, struct hubo_ref *r, struct timespec *t) {
 // ------------------------------------------------------------------------------
 
 		// Cheeting No more RAP or LAP
+
+/*
 		r->ref[RHP] = 0.0;
 		r->ref[LHP] = 0.0;
 		r->ref[RAP] = 0.0;
@@ -242,6 +244,7 @@ int runTraj(char* s, struct hubo_ref *r, struct timespec *t) {
 		r->ref[LAR] = 0.0;
 		r->ref[RHR] = 0.0;
 		r->ref[LHR] = 0.0;
+*/
 
         	ach_put( &chan_hubo_ref_filter, r, sizeof(*r));
 		//printf("Ref r = %s\n",ach_result_to_string(r));
