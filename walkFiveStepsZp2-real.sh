0056efb45164b27291c819b4f0a295d674246ced
Daniel M. Lofaro <dan@danlofaro.com>
2013-04-20 22:56:20 -0400

    added 0.1 and 0.05m walking

---
+++ walkFiveStepsZp2-real.sh
@@ -0,0 +1,5 @@
+sudo ./hubo-read-trajectory -f 200 -n walkReady.traj
+sudo ./hubo-read-trajectory -f 200 -n walkReadyOneSecWait.traj
+sudo ./hubo-read-trajectory -f 200 -n walkFiveSteps0p2m.traj
+sudo ./hubo-read-trajectory -f 200 -n walkReadyOneSecWait.traj
+sudo ./hubo-read-trajectory -f 200 -n home.traj
