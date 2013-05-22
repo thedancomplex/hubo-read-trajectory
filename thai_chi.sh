80d068d67033a8f37a7c1f901f5b66610a2ddffd
Daniel M. Lofaro <dan@danlofaro.com>
2013-05-22 14:36:00 -0400

    added thai_chi

---
+++ thai_chi.sh
@@ -0,0 +1,3 @@
+sudo ./hubo-read-trajectory -s -f 200 -n ./thaichi/home2walkready.traj
+sudo ./hubo-read-trajectory -s -f 200 -n ./thaichi/Taichi.traj
+sudo ./hubo-read-trajectory -s -f 200 -n ./thaichi/walkready2home.traj
