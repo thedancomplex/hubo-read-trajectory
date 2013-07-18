a9a106004cd43f7b03a3abf05ee82dd9069166af
Manas Paldhe <manaspaldhe12@gmail.com>
2013-07-17 20:32:21 -0400

    pause and play

--- src/hubo-read-trajectory.c
+++ src/hubo-read-trajectory.c
@@ -60,6 +60,17 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <inttypes.h>
 #include "ach.h"
 
+// for keyboard
+
+#include <termio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+
+
 //#include "../include/hubo_ref_filter.h"
 #include "hubo-ref-filter.h"
 
@@ -109,6 +120,15 @@ struct timeb {
 
 
 /* functions */
+// for keyboard
+static void tty_atexit(void);
+static int tty_reset(int);
+static void tweak_init();
+
+static struct termios save_termios;
+static int ttysavefd = -1;
+
+
 void stack_prefault(void);
 static inline void tsnorm(struct timespec *ts);
 void getMotorPosFrame(int motor, struct can_frame *frame);
@@ -195,6 +215,10 @@ int runTraj(char* s, struct hubo_ref *r, struct timespec *t) {
 		printf("No Trajectory File!!!\n");
 		return 1;  // exit if not file
 	}
+	char c;
+	bool paused=false;
+       	tweak_init();
+
 
 //	printf("Reading %s\n",s);
         while(fgets(str,sizeof(str),fp) != NULL) {
@@ -206,6 +230,22 @@ int runTraj(char* s, struct hubo_ref *r, struct timespec *t) {
 // ------------------------------------------------------------------------------
 // ---------------[ DO NOT EDIT AVBOE THIS LINE]---------------------------------
 // ------------------------------------------------------------------------------
+		if ( read(STDIN_FILENO, &c, 1) == 1) {
+        	         if (c=='p') {
+			paused=!paused;
+                	 }
+             	}
+
+		while (paused==true){
+			usleep(500000);//0.5seconds
+				if ( read(STDIN_FILENO, &c, 1) == 1) {
+			                if (c=='p') {
+						paused=!paused;
+                			 }
+            		 	}
+		}
+
+
 		int len = strlen(str)-1;
 		if(str[len] == "\n") {
 			str[len] = 0;
@@ -405,4 +445,70 @@ int main(int argc, char **argv) {
 //        pause();
         return 0;
 
+}
+
+
+
+// KEyboard Input
+
+static int
+tty_unbuffered(int fd) /* put terminal into a raw mode */
+{
+    struct termios buf;
+
+    if (tcgetattr(fd, &buf) < 0)
+        return(-1);
+
+    save_termios = buf; /* structure copy */
+
+    /* echo off, canonical mode off */
+    buf.c_lflag &= ~(ECHO | ICANON);
+
+    /* 1 byte at a time, no timer */
+    buf.c_cc[VMIN] = 1;
+    buf.c_cc[VTIME] = 0;
+    if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+        return(-1);
+
+    ttysavefd = fd;
+    return(0);
+}
+
+static int
+tty_reset(int fd) /* restore terminal's mode */
+{
+    if (tcsetattr(fd, TCSAFLUSH, &save_termios) < 0)
+        return(-1);
+    return(0);
+}
+
+static void
+tty_atexit(void) /* can be set up by atexit(tty_atexit) */
+{
+    if (ttysavefd >= 0)
+        tty_reset(ttysavefd);
+}
+
+static void
+tweak_init()
+{
+   /* make stdin unbuffered */
+    if (tty_unbuffered(STDIN_FILENO) < 0) {
+        printf("Set tty unbuffered error");// << std::endl;
+        //std::cout << "Set tty unbuffered error" << std::endl;
+        exit(1);
+    }
+
+    atexit(tty_atexit);
+
+    /* nonblock I/O */
+    int flags;
+    if ( (flags = fcntl(STDIN_FILENO, F_GETFL, 0)) == 1) {
+        perror("fcntl get flag error");
+        exit(1);
+    }
+    if (fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl set flag error");
+        exit(1);
+    }
 }
